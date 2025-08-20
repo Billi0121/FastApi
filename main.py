078ac7eb3623791e518c92c8fa336618e8dd0e9d
Billi0121 <gafarovbilol2009@gmail.com>
2025-08-20 06:13:24 -0500

    Cheking

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url='/gg')
 
 
 @app.get("/")
 def home_page():
     return {"Hello": "World"}
 
+@app.get("/{name}")
+def user_name(name: str):
+    return {"Hello": name.capitalize()}
